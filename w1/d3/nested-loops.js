b3bf7c3cb69625ee27cae4435c54bef47f9be50c
edmundtetteh <edmundtetteh2000@hotmail.com>
2023-05-17 12:40:27 -0600

    nested-loops

--- w1/d3/nested-loops.js
+++ w1/d3/nested-loops.js
@@ -1,39 +1,41 @@
 /*
 
+
+
   Nested Loops!
-  - sometimes a single loop is not enough to access every element
-  - so youd write a loop within a loop!
+
+  Sometimes a single just isn't enough to access a nested array, you may need a nested loop!
 
 */
 
-//outer loop pauses until inner loop can complete
-//since our inner loop is defined within the boundaries of our outer loop
-//the full cycle of inner loop is part of each individual iterations of our outer loop
-// for (let i = 0; i < 4; i++) {//outer loop is controlled by i, i cant increment until j finished
-//   console.log('outer loop:', i);
-//   for (let j = 0; j < 3; j++) {//inner loop is controlled by j, j will iterate after each increment of i
-//     console.log('inner loop:  ', j);
+// outer loop pauses until the inner loop can complete, then it will resume
+// since our inner loop is defined within the bounds of our outer loop, the full cycle of inner loop must complete
+
+// for (let i = 0; i < 4; i++) {
+//   //outer loop is controlled by i
+//   console.log("i:", i);
+//   for (let j = 0; j < 3; j++) {
+//     //inner loop is controlled by j
+//     console.log("j:   ", j);
 //   }
 // }
 
-//matrix is another name for 2d array
 let matrix = [
   ["a", "b", "c"],
   ["d", "e", "f"],
+  ["g", "h", "i"],
 ];
 
-/*
-i = 0
-matrix[0] = [ 'a', 'b', 'c' ]
-*/
+console.log(matrix);
+for (let i = 0; i < matrix.length; i++) {
+  let subArr = matrix[i];
+  // console.log("subArr:", subArr);
 
-//gain access to every letter
-for(let i = 0; i < matrix.length; i++) {
-  let subArr = matrix[i];//["d", "e", "f"]
-  console.log('subArr', subArr);
-
-  for(let j = 0; j < subArr.length; j++) {
-    let letter = subArr[j]; 
-    console.log('letter   ',letter);
+  for (let j = 0; j < subArr.length; j++) {
+    let letter = matrix[i][j]
+    console.log(letter)
+    // let letter2 = matrix[i][j]; // same thing as above
+    // console.log(i, j)
+    // console.log("letter:  ", letter);
   }
 }
