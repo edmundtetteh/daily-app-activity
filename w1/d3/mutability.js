0d134919a97c290a14c1c830510b683ddbd8ee5d
edmundtetteh <edmundtetteh2000@hotmail.com>
2023-05-17 11:28:53 -0600

    array-methods

--- w1/d3/mutability.js
+++ w1/d3/mutability.js
@@ -1,37 +1,37 @@
 /*
 
 
-  Mutability
-  What is mutability?
-  - can be changed or altered, changeable, mutated
 
-  Mutable - can be changed
+
+  Mutability! - is the concept of certain pieces of data being able to be mutated. Of the datatypes we've seen so far that can be mutated are arrays.
+
+  Mutable - can change element
     - arrays
-    - objects - learn next week!
+    - objects
 
   Immutable - cannot be changed
-    - string
+    - strings
     - numbers
     - booleans
-
 */
 
-//arrays are mutable
-let nums = [1,2,3];
-console.log(nums);
-//use the assignment operator - =, reassign the value in a array, therefore mutating it
-nums[0] = 7;// saying hey, in the nums array at index 0, reassign 1 to now be 7
-console.log(nums);//[ 7, 2, 3 ]
-//we have mutated our array
 
-//strings are not mutable!
+let sequence = ['T','A','C','G'];
+// console.log(sequence);//[ 'T', 'A', 'C', 'G' ]
+sequence[0] = 'C';
+console.log(sequence);//[ 'C', 'A', 'C', 'G' ]
+// were able to modify or mutate the array by keying into and altering whats there
+// we are mutating this sequence
+
+//strings may seem mutable but they are not
 let name = 'brandon';
-console.log(name);//brandon
+console.log(name);// brandon
 name[0] = 'k';
-console.log(name);//brandon
-//why? because strings are not mutable! you cant mutate
-//the only thing we can do, is reassign the entire string
-name = 'krandon';
+console.log(name);// brandon
 
+//we cant mutate a string
+//all we can do, is reassign the string
+name = 'krandon';//have to reassign name to a whole new string
 console.log(name);//krandon
+
 
