29c2c8eb0e59e163d4cbb85f96863fdb2a835b3e
edmundtetteh <edmundtetteh2000@hotmail.com>
2023-05-27 21:43:05 -0600

    closures and scope

--- w2/d4/closures.js
+++ w2/d4/closures.js
@@ -1,122 +1,106 @@
 /*
 
 
+
+
   Closures!
-  - when a inner function uses or changes the values of the outer
+  - when a inner function uses or changes the value of the outer function
   - when a parent function cant access data in the child, but the child can access the parent
-  - closure have access to any variable within its own scope as well as any outer functions
-  - a closure will keep reference to all its variables defined even if its outer function has returned
-  - inner func closes over the outer func
+  - closure have access to any variables within its own scope as well as any outer functions
+  - a closure will keep reference to all its variables defined even if its outer functions has returned
+  - inner function is closing over the outer func
 
-  Advantages of closures?
-    - private state
-    - data encapsulation
-    - currying
+  ? Advantages of closures?
+  1. private state
+  2. data encapsulation
+  3. currying
 
-  Disadvantage
-    - variable are not garbage collected because stored in the lexical environment
-    - slow down code if you use too many
+  ? Disadvantages of closures?
+  1. memory intensive - variables are not garbage collected, because they are stored in lexical env
+  2. slow down code if you too many
 
-  Lexical environment
-  - the lexical environment consists of any variables available within scope in which the closure was declared
-  - anytime a function defined, a lexical env is created and attached to the function under the hood, this happens during parsing time
-
+  ! Lexical Environment
+    - The lexical Environment consist of any variables available within scope in which the closure was declared
+    - anytime a function is defined, a lexical env is created and attached to the function under the hood
 */
 
 
 //first example
-//this shows that we have access to outer function variable from an inner func
+//this shows that we have access to outer function variable from inner func
 function pizzaMaker(food) {
 
   let order = 'id like a pizza with ';
 
-  function oven() {
+  function oven(){
     return order + food;
   };
 
   return oven();
 };
 
-// console.log(pizzaMaker('cheese'));
+// console.log(pizzaMaker('cheese'));//id like a pizza with cheese
 
-//second example
-//show that we can access and modify variables of functions that are closed over
-function groceryList(list) {
-
-  let groceries = list;
-
-  function addItem() {
-    groceries.push('ice cream');
-  };
-
-  addItem();
-  return groceries;
-};
-
-// console.log(groceryList(['milk', 'eggs']));//[ 'milk', 'eggs', 'ice cream' ]
-
-
-//show that even after the function has returned we still have access to the outer funcs variables
-//can return functions!!
 //Why are functions first class objects
 //1. stored in variable - last thursday
 //2. passed as an argument - learnt yesterday
 //3. can be returned from a function.
 
+//shows that even after the function has returned we still have access to the outer funcs variables
 
-function elephantCollector() {
-
+function elephantCollector(){
   let elephants = ['dumbo'];
 
   let myFunc = function(name) {
+
     elephants.push(name);
-    return elephants;
+    return elephants
   };
 
   return myFunc;
-};
-
-// console.log(elephantCollector);//[Function: elephantCollector]
+}
 
 //elephant state 1
-let elephantParade = elephantCollector();
+// console.log(elephantCollector)
+//[Function: elephantCollector]
+// console.log(elephantCollector());//[Function: myFunc]
+let elephantParade = elephantCollector();// storing the return value of elephantCollector which is a function that we can call
 // console.log(elephantParade);//[Function: myFunc]
-//invoking the inner function as passing argument
 // console.log(elephantParade('babar'));//[ 'dumbo', 'babar' ]
 
-//two different states
-
-//elephant state 1
-let elephantParade2 = elephantCollector();
-// console.log(elephantParade2);
+//elephant state
+//a separate state
+// let elephantParade2 = elephantCollector();
 // console.log(elephantParade2('henry'));//[ 'dumbo', 'henry' ]
-// console.log(elephantCollector()('babar2'));
+// console.log(elephantCollector(),('what will this print'));//[ 'dumbo', 'what will this print' ]
+// console.log(elephantParade2('brandon'));//[ 'dumbo', 'henry', 'brandon' ]
 
 
 
-//emulate private function and variables
+//emulate private functions variables
 function createCounter(){
   let count = 0;
 
-  return function() {
+  return function(n) {
+    n= count
     count++;
     return count;
   }
 }
 
 //create a private state 1
-const counter1 = createCounter();
-// console.log(counter1)
-console.log(counter1());
-console.log(counter1());
-console.log(counter1());
+console.log(createCounter);//[Function: createCounter]
+console.log(createCounter());//[Function (anonymous)]
+let counter1 = createCounter();
+console.log(counter1);//[Function (anonymous)]
+console.log(counter1());//1
+console.log(counter1());//2
+
+//create a private state 2
+let counter2 = createCounter();
+console.log();//[Function (anonymous)]
+console.log(counter2());//1
+console.log(counter2());//2
 
 
-//create a private start 2
-console.log('counter 2')
-const counter2 = createCounter();
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
-// console.log(counter1 == counter2);//false
-//
+// return function - would return the function itself
+// return function() - would return the return value of the function
